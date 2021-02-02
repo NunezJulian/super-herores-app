e23a3a2baae3873dd6bb30109b4de031663f9267
Julian Nuñez <julian@seistreinta.com>
2021-02-01 19:36:32 -0700

    okhttp client and trust manager for certificates added

--- app/src/main/java/com/example/superheroes/app/Services.kt
+++ app/src/main/java/com/example/superheroes/app/Services.kt
@@ -1,9 +1,14 @@
 package com.example.superheroes.app
 
+import android.os.Build
 import com.google.gson.Gson
 import com.google.gson.GsonBuilder
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.security.SecureRandom
+import java.security.cert.X509Certificate
+import javax.net.ssl.*
 
 class Services {
 
@@ -11,14 +16,50 @@ class Services {
     val BASE_URL: String = "https://superheroapi.com/api/10156112965520834/"
     val ALL_HEROES_BASE_URL = "https://akabab.github.io/superhero-api/api/"
 
+
+
+
     fun getRetrofitInstance(showAllHeroes: Boolean): Retrofit {
+        val okHttpClient: OkHttpClient = unSafeOkHttpClient()
+            .build()
+
         val gson: Gson = GsonBuilder()
             .setDateFormat("yyyy-MM-dd'T'HH:mm:ss")
             .create()
 
         return Retrofit.Builder()
             .baseUrl(if (showAllHeroes) ALL_HEROES_BASE_URL else BASE_URL)
+            .client(okHttpClient)
             .addConverterFactory(GsonConverterFactory.create(gson))
             .build()
     }
+
+    fun unSafeOkHttpClient() :OkHttpClient.Builder {
+        val okHttpClient = OkHttpClient.Builder()
+        try {
+            // Create a trust manager that does not validate certificate chains
+            val trustAllCerts:  Array<TrustManager> = arrayOf(object : X509TrustManager {
+                override fun checkClientTrusted(chain: Array<out X509Certificate>?, authType: String?){}
+                override fun checkServerTrusted(chain: Array<out X509Certificate>?, authType: String?) {}
+                override fun getAcceptedIssuers(): Array<X509Certificate>  = arrayOf()
+            })
+
+            // Install the all-trusting trust manager
+            val  sslContext = SSLContext.getInstance("SSL")
+            sslContext.init(null, trustAllCerts, SecureRandom())
+
+            // Create an ssl socket factory with our all-trusting manager
+            val sslSocketFactory = sslContext.socketFactory
+            if (trustAllCerts.isNotEmpty() &&  trustAllCerts.first() is X509TrustManager) {
+                okHttpClient.sslSocketFactory(sslSocketFactory, trustAllCerts.first() as X509TrustManager)
+                okHttpClient.hostnameVerifier(HostnameVerifier { s, sslSession -> true })
+            }
+
+            return okHttpClient
+        } catch (e: Exception) {
+            return okHttpClient
+        }
+    }
+
+
 }
